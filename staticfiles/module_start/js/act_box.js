84ff344c9f5dfa634f554d957002a449b991c409
Octavio Cubillos <ocubillosj@gmail.com>
2017-10-25 10:31:33 -0300

    test

---
+++ staticfiles/module_start/js/act_box.js
@@ -0,0 +1,26 @@
+
+    ClassicEditor
+        .create( document.querySelector( '#editor1' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+    ClassicEditor
+        .create( document.querySelector( '#editor2' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+    ClassicEditor
+        .create( document.querySelector( '#editor3' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+    ClassicEditor
+        .create( document.querySelector( '#editor4' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+    ClassicEditor
+        .create( document.querySelector( '#editor5' ) )
+        .catch( error => {
+            console.error( error );
+        } );
